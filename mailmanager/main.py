a6b0cb9abdaee23d5d0c7bb47e1054f78873fc78
moamoak <kervella.mael@gmail.com>
2017-08-13 23:58:03 +0200

    Edit user page The user can edit its own name and its own password.

--- mailmanager/main.py
+++ mailmanager/main.py
@@ -4,6 +4,7 @@
 import os
 import sqlite3
 import time
+from random import randint
 from hashlib import sha256
 from flask import Flask, request, session, g, redirect, url_for, abort, \
              render_template, flash
@@ -109,6 +110,81 @@ def add_mailbox():
 
 
 
+@app.route('/edituser/', methods=['GET', 'POST'])
+def edit_user():
+    """The page where a user can edit it's personal infos"""
+
+    user_id = session.get('user_id')
+    if not user_id :
+        return redirect(url_for('login'))
+
+    # Errors about the name or the password
+    error_name = None
+    error_pass = None
+
+    # Get the actual informations stored
+    db = get_db()
+    cur = db.execute('SELECT id, username FROM users WHERE id=?', [user_id])
+    user = cur.fetchone()
+
+    # Does the user exists or did sth went wrong ?
+    # We force logout to be sure it's not an old session
+    if not user:
+        flash('Something went wrong : it seems your account doesn\'t exists anymore')
+        return logout()
+
+    # The POST method is used (= the user has filled the form with new infos)
+    if request.method == 'POST':
+        # Has the username been changed ?
+        if request.form['username'] and request.form['username'] != user['username']:
+            username = request.form['username']
+            # Trying to change the name in the database
+            try :
+                db.execute('UPDATE users SET username=? WHERE id=?',
+                        [username, user_id])
+                db.commit()
+            except :
+                error_name = 'Something went wrong while modifying your username'
+                log(sys.exc_info())
+            else:
+                log('Username changed from '+user['username']+' to '+username)
+                flash ('Your username has successfully been changed to '+username)
+
+
+        # Has the password been changed ?
+        if request.form['password1'] or request.form['password2'] :
+            if not request.form['password1'] or not request.form['password2'] :
+                # One of the two password field is empty
+                error_pass = 'Please fill in both password field'
+            elif request.form['password1'] != request.form['password2'] :
+                # Both password field don't match
+                error_pass = 'Password don\'t match'
+            else :
+                # Everything is allright let's get password and salt ready
+                password = request.form['password1']
+                salt = randint(1000000, 1000000000)
+                # Trying to actually modify the db
+                try :
+                    db.execute('UPDATE users SET password=?, salt=? WHERE id=?',
+                            [saltpassword(password, salt), salt, user_id])
+                    db.commit()
+                except :
+                    error_pass = 'Something went wrong while changing your password'
+                    log(sys.exc_info())
+                else :
+                    log(user['username']+'s password modified')
+                    flash('Your password has been successfully modified')
+
+    # In case something has changed we reload data from the db
+    # So we're sure to be up to date
+    user = db.execute('SELECT id, username FROM users WHERE id=?', [user_id]).fetchone()
+
+    # Back to the edituser page but with error or success displayed
+    return render_template('edituser.html', user=user, error_name=error_name, error_pass=error_pass)
+
+
+
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -122,7 +198,7 @@ def login():
         if request.form['username'] and request.form['password'] :
             # Get infos from database
             db = get_db()
-            cur = db.execute('SELECT username, password, salt FROM users WHERE username=?',
+            cur = db.execute('SELECT id, username, password, salt FROM users WHERE username=?',
                     [request.form['username']])
             user = cur.fetchone()
 
@@ -132,7 +208,7 @@ def login():
             else :
                 # Is the given password the correct one
                 if saltpassword(request.form['password'], user['salt']) == user['password'] :
-                    session['logged_in'] = True
+                    session['user_id'] = user['id']
                     log('User '+request.form['username']+' connected')
                     flash('Successfully connected')
                     # back to the welcome page
@@ -149,6 +225,6 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('You were logged out')
     return redirect(url_for('login'))
