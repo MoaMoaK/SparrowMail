25a63a25d33137485be1ed459a3870e1f2d953a8
moamoak <kervella.mael@gmail.com>
2017-09-02 23:55:52 +0200

    Forgotten file : error.py -> objects to manage errors Should have been commited with 7be964fd

---
+++ mailmanager/error.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+class ErrorCategory :
+    def __init__( self, name ) :
+        self.name = name
+
+    def getName( self ) :
+        return self.name
+
+MissingArg  = ErrorCategory( 'Missing argument'        )
+WrongArg    = ErrorCategory( 'Wrong argument'          )
+DBManip     = ErrorCategory( 'Database manipulation'   )
+SieveManip  = ErrorCategory( 'Sieve file manipulation' )
+SieveSyntax = ErrorCategory( 'Sieve syntax'            )
+Hacker      = ErrorCategory( 'R U H4ck3r ?'            )
+Unknown     = ErrorCategory( ''                        )
+ConfPasswd  = ErrorCategory( 'Confirmation password'   )
+
+class Error :
+    def __init__( self, cat, info ) :
+        self.cat = cat
+        self.info = info
+
+    def getCategory( self ) :
+        return self.cat
+
+    def getInfo( self ) :
+        return self.info
+
+    def message( self ) :
+        catName = self.getCategory().getName()
+        if catName == '' :
+            return '<strong>Error :</strong> '+self.getInfo()
+        else :
+            return '<strong> Error ('+self.getCategory().getName()+') :</strong> '+self.getInfo()
