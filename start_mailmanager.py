d9e7d31c2d6fef7c7b675891342d1f35ae9ac8f7
moamoak <kervella.mael@gmail.com>
2017-09-03 21:15:13 +0200

    Run the server locally if no server_name specified

--- start_mailmanager.py
+++ start_mailmanager.py
@@ -1,6 +1,10 @@
 from sparrowmail import app
 
-server_name = app.config['SERVER_NAME'].split(':')
-host = server_name[0]
-port = int(server_name[1])
-app.run(host=host, port=port)
+server_name = app.config['SERVER_NAME']
+if server_name :
+    split_name = server_name.split(':')
+    host = split_name[0]
+    port = int(split_name[1])
+    app.run(host=host, port=port)
+else :
+    app.run()
