ca59d1dfe73154e53abca586751cfac5a7d405b8
moamoak <kervella.mael@gmail.com>
2017-09-03 19:54:12 +0200

    Default user at db init The default user has usernmae="user" and password="password" Thoses are for installation purpose and are supposed to be changed as sson as possible

--- sparrowmail/db/schema.sql
+++ sparrowmail/db/schema.sql
@@ -6,6 +6,9 @@ CREATE TABLE users (
     salt text not null
 );
 
+INSERT INTO users (username, password, salt)
+VALUES ("user", "b2d1349ea3f52597f37566ed4095e10729c8329c612b64563849dcf29ee9de6e", "123456789");
+
 DROP TABLE IF EXISTS mails;
 CREATE TABLE mails (
     id           integer primary key autoincrement,
