921973de37f22d223ea3b15bf0395c78f8267a1e
moamoak <kervella.mael@gmail.com>
2017-09-04 01:35:28 +0200

    Typo

--- start_sparrowmail.py
+++ start_sparrowmail.py
@@ -3,7 +3,7 @@ from sparrowmail import app
 server_host = app.config['SERVER_HOST']
 server_port = app.config['SERVER_PORT']
 
-if server_name :
+if server_host :
     if server_port :
         app.run(host=server_host, port=server_port)
     else :
