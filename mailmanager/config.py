b6a2e4a69c65e39703402c604817e53e11d18254
moamoak <kervella.mael@gmail.com>
2017-09-03 03:32:23 +0200

    Flask config options added to config file

--- mailmanager/config.py
+++ mailmanager/config.py
@@ -7,54 +7,67 @@
 
 
 
-## Security Warning
-## Those options should be set to False
-## if not in development environnement
+## Flask configuration ##
+#########################
+#   Debugging options
+#   /!\ Security Warning not use those in dev env
 DEBUG = True
 TESTING = False
-
-
-
-## Main database
-
-# -> Where the database is store
-DATABASE = 'mailmanager/db/mailmanager.db'
-# ->
+EXPLAIN_TEMPLATE_LOADING = False
+#   Secret key for encryption
 SECRET_KEY = 'development-key'
+#   Cookies configuration
+SESSION_COOKIE_NAME = 'session'
+SESSION_COOKIE_DOMAIN = 'mail.maelkervella.eu'
+SESSION_COOKIE_PATH = '/'
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = False
+SESSION_REFRESH_EACH_REQUEST = True
+#   Server hostname and port
+SERVER_NAME = 'mail.maelkervella.eu:5000'
+#   Website root url
+#   None if sub-domain is only used for MailManager
+#   /<path>/<to>/<root>/ if not
+APPLICATION_ROOT = None
 
 
 
-## Files for postfix virtual accounts
 
-# -> aliases associations 
-#    in postix conf : virtual_alias_map
+## Main database ##
+###################
+#   Where the database is store
+DATABASE = 'mailmanager/db/mailmanager.db'
+
+
+
+## Files for postfix virtual accounts ##
+########################################
+#   aliases associations 
+#   in postix conf : virtual_alias_map
 ALIASES_FILE_PATH = '/tmp/aliases'
-
-# -> mailboxes addresses
-#    in postfix conf : virtual_mailbox_maps
+#   mailboxes addresses
+#   in postfix conf : virtual_mailbox_maps
 MAILBOXES_FILE_PATH = '/tmp/mailboxes'
 
 
 
-## Files for dovecot management
-
-# -> mailboxes passwords
-#    in dovecot conf : passdb { args = username_format=%u scheme=ssha512 <file> }
+## Files for dovecot management ##
+##################################
+#   mailboxes passwords
+#   in dovecot conf : passdb { args = username_format=%u scheme=ssha512 <file> }
 PASSWD_FILE_PATH = '/tmp/passwd.db'
 
 
 
-## Sieve handling configuration
-
-# -> Where the virtual mailboxes are stored
-#    Tree must look like <VMAIL_DIR>/<domain>/<user>/mail/{cur|new|tmp}
+## Sieve handling configuration ##
+##################################
+#   Where the virtual mailboxes are stored
+#   Tree must look like <VMAIL_DIR>/<domain>/<user>/mail/{cur|new|tmp}
 VMAIL_DIR = '/tmp/vmail'
-
-# -> List of directories to exclude when exploring the virtual mailboxes tree
-#    because they are not assoicated with a domain or a user for example
+#   List of directories to exclude when exploring the virtual mailboxes tree
+#   because they are not assoicated with a domain or a user for example
 EXCLUDE_DIRS = ['sieve-after', 'sieve-after']
-
-# -> The name of the sieve files
-#    in dovecot conf : plugin { sieve = <file> }
+#   The name of the sieve files
+#   in dovecot conf : plugin { sieve = <file> }
 SIEVE_FILENAME = '.dovecot.sieve'
 
