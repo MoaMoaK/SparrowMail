08e72a127e6c32a60e8e7b24e3e11bbe3092b37d
moamoak <kervella.mael@gmail.com>
2017-09-03 21:07:44 +0200

    Update version and package data of the app

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup
 
 setup(
     name='sparrowmail',
+    version='0.1',
     packages=['sparrowmail'],
+
     include_package_data=True,
+
     install_requires=[
         'sparrowmail',
         'email_validator',
