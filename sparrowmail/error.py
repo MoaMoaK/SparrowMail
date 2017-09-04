e58b67d730d9df9d34ad56ab071b27265cf91df7
moamoak <kervella.mael@gmail.com>
2017-09-05 00:24:08 +0200

    Typo : missing '

--- sparrowmail/error.py
+++ sparrowmail/error.py
@@ -34,4 +34,4 @@ class Error :
         if catName == '' :
             return '<strong>Error :</strong> '+self.getInfo()
         else :
-            return '<strong> Error ('+self.getCategory().getName()+') :</strong> '+self.getInfo().replace('\n', '<br>)
+            return '<strong> Error ('+self.getCategory().getName()+') :</strong> '+self.getInfo().replace('\n', '<br>')
